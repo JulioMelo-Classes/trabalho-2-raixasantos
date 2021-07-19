8be783bc6cf2f6cbc918be71c40527b88091728b
Raíssa Santos <raixapsantos@gmail.com>
2021-07-18 22:28:27 -0300

    Implementação da classe CanalTexto

--- include/canaltexto.h
+++ include/canaltexto.h
@@ -1,10 +1,26 @@
 #ifndef CANALTEXTO_H
 #define CANALTEXTO_H
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "mensagem.h"
+
+using namespace std;
 
 class CanalTexto{
-	/*
-		TODO implementação dos atributos e métodos
-	*/
+	private:
+		string nome; //<! Nome do canal.
+		std::vector<Mensagem> mensagens; //<! Vetor com mensagens do canal.
+	public:
+		/*! Altera o nome do canal.
+			@param nome_ O nome.
+			@return void */
+		void set_nome(string nome_);
+
+		/*! Recupera o nome do canal.
+			@return O nome. */
+		string get_nome() const;
 };
 
 #endif
